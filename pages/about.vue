2ce0d456b472162b9d4ddce62512d6a678d35c35
Steve <srp2120@gmail.com>
2019-09-12 20:39:34 -0700

    new about input

--- pages/about.vue
+++ pages/about.vue
@@ -8,12 +8,12 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Fake Heading</h3>
-          <p>
+          <h3>I'm just a problem solver for fun.</h3>
+          <!--<p>
             <strong>
               Edit this page in<code>/pages/about.vue</code>to fit your needs.
             </strong>
-          </p>
+          </p>-->
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
             eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
@@ -21,28 +21,31 @@
             ratione rem repudiandae sit!
           </p>
 
-          <h3>Another Fake Heading</h3>
+          <h3>A Jack of all trades and a master of some.</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
+            I've always sought after knowing as much about everything I could. My brain takes a view from above. A look at the BIG picture. 
           </p>
-          <quote-card />
-          <h3>Still More Fake Content</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit.
-            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
-            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
-            dicta ipsa iste modi provident, quod, similique veritatis.
+            Then, I give it my best shot. 
+          </p>
+          <p>
+            It's not really something to brag about, honestly. Everyone thinks they have the best answers. There's really on solace in the fact that you can be a part of a growing community of those who genuinely thrive to test if they have the best answers. 
+          </p>
+          <!--<quote-card />-->
+          <h3>Fun stuff to find in this blog</h3>
+          <p>
+            My future plans for this blog are to share my thoughts and ideas as well as to listen to your comments about them. The world needs all of us to solve all of the world's problems. You can expect: 
+            <ul style="list-style-type:disc;">
+              <li>Meta posts about how best to format these blog posts.</li>
+              <li>Project posts of serious ideas that I want to implement.</li>
+              <li>Philosophical posts about the answers to the BIG questions.</li>
+              <li>Rant posts, hopefully twisted into the form of civil community efforts.</li>
+              <li>Entertainment posts to include: <ul style="list-style-type:disc;"><li>music</li><li>voice acting</li><li>amateur drawing</li><li>story writing</li> Much of which will be open source licenced and requesting talented contributions from the community.</li>
+            </ul>   
           </p>
         </div>
 
-        <div class="tile is-ancestor">
+        <!--<div class="tile is-ancestor">
           <div class="tile is-parent">
             <article class="tile is-child box">
               <p class="title">
@@ -82,7 +85,7 @@
               </figure>
             </article>
           </div>
-        </div>
+        </div>-->
       </template>
 
       <template v-slot:sidebar>
@@ -95,13 +98,13 @@
   </div>
 </template>
 <script>
-import QuoteCard from '~/components/cards/QuoteCard'
-export default {
-  head() {
-    return {
-      title: `About | ${this.$siteConfig.siteName}`
-    }
-  },
-  components: { QuoteCard }
-}
+// import QuoteCard from '~/components/cards/QuoteCard'
+//export default {
+//  head() {
+//    return {
+//      title: `About | ${this.$siteConfig.siteName}`
+//    }
+//  },
+//  components: { QuoteCard }
+//}
 </script>
